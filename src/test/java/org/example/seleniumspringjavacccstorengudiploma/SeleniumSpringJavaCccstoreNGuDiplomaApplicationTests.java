c6cd3bae215b1eef488399809eb5a24ada5dd00d
NataliaGumarova <n.gu.47943@gmail.com>
2025-10-28 14:41:23 +0400

    Дипломный проект "Мануальное и автоматизированное тестирование сайта cccstore.ru. Проектирование и разработка базы данных с помощью MySQL"

---
+++ src/test/java/org/example/seleniumspringjavacccstorengudiploma/SeleniumSpringJavaCccstoreNGuDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seleniumspringjavacccstorengudiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumSpringJavaCccstoreNGuDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
